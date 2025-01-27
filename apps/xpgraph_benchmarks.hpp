6420154af956c985f978ce7eb6931948be084d53
Long Deng <ldeng@mail.ustc.edu.cn>
2025-01-27 08:28:17 +0000

    add rss

--- apps/xpgraph_benchmarks.hpp
+++ apps/xpgraph_benchmarks.hpp
@@ -10,6 +10,27 @@
 #include "pr_gapbs.hpp"
 #include "connected_components.hpp"
 
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+long get_rss() {
+    std::ifstream stat_file("/proc/self/stat");
+    std::string line;
+    std::getline(stat_file, line);
+    std::istringstream iss(line);
+    std::string token;
+
+    // 跳过前 23 个字段
+    for (int i = 0; i < 23; ++i) {
+        iss >> token;
+    }
+
+    // 读取第 24 个字段（RSS）
+    iss >> token;
+    return std::stol(token) * sysconf(_SC_PAGESIZE);
+}
+
 inline double mywtime()
 {
 	double time[2];	
@@ -27,17 +48,22 @@ inline double mywtime()
 #endif
 
 void test_graph_benchmarks_gapbs(XPGraph* xpgraph){
+    long rss_ingest = get_rss();
+
     auto st = mywtime();
     for(size_t i=0; i<20; i++)
         test_bfs_numa(xpgraph, i);
     auto ts1 = mywtime();
+
+    long rss_bfs = get_rss();
 
     auto res = pr_gapbs(xpgraph, 10);
     auto ts2 = mywtime();
     delete [] res;
 
     auto ts3_st = mywtime();
-    test_connected_components_numa(xpgraph, 2);
+    for(size_t i=0; i<10; i++)
+        test_connected_components_numa(xpgraph, 2);
     auto ts3 = mywtime();
     
     auto t_bfs = ts1 - st;
@@ -47,4 +73,7 @@ void test_graph_benchmarks_gapbs(XPGraph* xpgraph){
     printf(EXPOUT "BFS: %.3fs\n", t_bfs);
     printf(EXPOUT "PR: %.3fs\n", t_pr);
     printf(EXPOUT "CC: %.3fs\n", t_cc);
+
+    printf(EXPOUT "RSS_Ingest: %ld\n", rss_ingest);
+    printf(EXPOUT "RSS_BFS: %ld\n", rss_bfs);
 }
